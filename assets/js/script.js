b262d89a0b461b04ed02b0da2433bda5454a8d54
dahan david <david8dahan@gmail.com>
2021-07-24 12:40:26 +0200

    améliorations js et css

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,34 @@ AOS.init({
   });
 
 
+// Function strobe pour mettre un id en clignotement
+
+var arrayColors = new Array("rgb(255,255,255,1)","rgb(255,255,255,0.95)","rgb(255,255,255,0.90)","rgb(255,255,255,0.85)","rgb(255,255,255,0.80)","rgb(255,255,255,0.75)","rgb(255,255,255,0.70)","rgb(255,255,255,0.65)","rgb(255,255,255,0.60)","rgb(255,255,255,0.55)","rgb(255,255,255,0.50)","rgb(255,255,255,0.45)","rgb(255,255,255,0.40)","rgb(255,255,255,0.35)","rgb(255,255,255,0.30)","rgb(255,255,255,0.25)","rgb(255,255,255,0.20)","rgb(255,255,255,0.15)","rgb(255,255,255,0.10)","rgb(255,255,255,0)");
+
+strobeEffect();
+
+
+var indiceColorIntense=0;
+var incrementTwoDirections=1;
+
+ function strobeEffect(){
+    color=arrayColors[indiceColorIntense];
+    // on change le signe de l'increment pour balayer le tableau dans un sens puis dans l'autre
+    indiceColorIntense+=incrementTwoDirections;if (indiceColorIntense==20) incrementTwoDirections-=2;if (indiceColorIntense==0) incrementTwoDirections+=2;
+    // vitesse du fading se regle ici en ms
+    xx=setTimeout("strobeEffect()",200);  
+    
+    var elementsStrobing = document.getElementsByClassName('strobe')
+    for (let i = 0 ; i < elementsStrobing.length ; i++ ){
+        elementsStrobing[i].style.color = color;
+    }
+    // document.getElementById('strobe').style.color = color;
+}
+
+
+// Fin function strobe
+  
+
 //animation fleche pour revenir au top
 window.addEventListener('scroll', function(){
     // window.scrollY nous permet d'avoir la distance entre le haut et le scroll effectué, c'est une valeur en px
